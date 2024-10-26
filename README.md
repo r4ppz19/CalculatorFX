61c66832dcc015db2e95ec406e6cabc4e9317b14
John Rey Rabosa <johnreyrabosa18@gmail.com>
2024-10-26 20:23:45 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Calculator Application
+
+This is a simple calculator application built using JavaFX. The application supports basic arithmetic operations such as addition, subtraction, multiplication, and division.
+
+
+
+![Calculator Image](src/main/resources/image/Calculator.png)
+
+
+
+## Features
+
+- Basic arithmetic operations: addition, subtraction, multiplication, and division.
+- User-friendly interface with buttons for digits and operations.
+- Error handling for invalid inputs.
+- Clear and delete functionalities.
+- Toggle sign functionality.
+
+## How to Run
+
+1. Clone the repository.
+2. Open the project in IntelliJ IDEA or any other Java IDE.
+3. Run the `Main.java` file to start the application.
+
+## Dependencies
+
+- JavaFX: Ensure you have JavaFX SDK installed and configured in your IDE or use the Zulu JDK which includes JavaFX.
+#
+#
+### **CREATOR: R4PPZ**
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,33 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("src/main/resources/fxml/MainView.fxml")));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Calculator");
+
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("src/main/resources/css/ModernMinimalistStyle.css")).toExternalForm());
+
+        Image imageIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("src/main/resources/image/mainIcon.png")));
+        primaryStage.getIcons().add(imageIcon);
+
+        primaryStage.show();
+    }
+}
